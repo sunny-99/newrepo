c6ee138b04f340bca99e8c0c588a13f74e47e9ff
sunny-99 <sunnyshah480@gmail.com>
2022-03-17 02:04:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         
-        System.out.println("Git And Jenkins 1..2..3..4");
+        System.out.println("Git And Jenkins 1..2..3");
     }
 }
